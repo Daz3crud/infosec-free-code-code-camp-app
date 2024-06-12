7d43a0bb8af06c6e66707796e9ab2387758d21ab
Daz3Crud <lukydaze@gmail.com>
2024-06-12 17:23:32 -0400

    version2 yarn

--- myApp.js
+++ myApp.js
@@ -2,63 +2,30 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+// Use helmet middleware
+app.use(helmet());
 
-// my code 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
-app.disable('strict-transport-security');
+
+// Import API routes from server.js
+const api = require('./server.js');
+
+// Use API routes
 app.use('/_api', api);
+
+// Disable strict-transport-security
+app.disable('strict-transport-security');
+
+// Serve the main page
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+// Define the port to listen on
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Useful programmer Infoo Securyty App Started on Port ${PORT}`);
-
-
+  console.log(`Useful programmer Info Security App Started on Port ${port}`);
 });
+
+module.exports = app;
