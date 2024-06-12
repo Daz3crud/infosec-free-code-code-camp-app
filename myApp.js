03eb908f966149f2ab1e40fe45b9e80af3a2d518
Daz3Crud <lukydaze@gmail.com>
2024-06-12 18:51:06 -0400

    added

--- myApp.js
+++ myApp.js
@@ -7,10 +7,43 @@ app.use(helmet.hidePoweredBy());
 
 
 
-// Define the port to listen on
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App started on port ${port}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
